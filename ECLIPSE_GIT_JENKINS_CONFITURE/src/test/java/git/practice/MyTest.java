2dfff31eb6138c450d68a3b393d5f6c51dc41893
Bhupal Poudyal <poubhupal@gmail.com>
2023-10-15 08:08:31 -0400

    This is my first commit from eclipse to Git Repository

---
+++ ECLIPSE_GIT_JENKINS_CONFITURE/src/test/java/git/practice/MyTest.java
@@ -0,0 +1,36 @@
+package git.practice;
+
+import org.testng.annotations.Test;
+
+public class MyTest {
+	
+	
+	@Test
+	
+	public static void practiceTest() {
+		System.out.println("Thisis my  test");
+	}
+	
+@Test
+	
+	public static void practiceTest1() {
+		System.out.println("Thisis my first test");
+	}
+	
+
+@Test
+
+public static void practiceTest2() {
+	System.out.println("Thisis my Second test");
+}
+
+
+@Test
+
+public static void practiceTest3() {
+	System.out.println("Thisis my Third test");
+}
+
+	
+
+}
