5894546b82bc40ca2f6ef62e1f443b894167d53c
Bhupal Poudyal <poubhupal@gmail.com>
2023-10-15 09:53:31 -0400

    This is forth commit with new project

---
+++ Eclipse_git_jenkins/src/test/java/git/practice/MyTest.java
@@ -0,0 +1,58 @@
+package git.practice;
+
+import org.testng.annotations.Test;
+
+public class MyTest {
+	
+	
+	@Test
+	
+	public static void practiceTest() {
+		System.out.println("This is my  test");
+	}
+	
+@Test
+	
+	public static void practiceTest1() {
+		System.out.println("This is my first test");
+	}
+	
+
+@Test
+
+public static void practiceTest2() {
+	System.out.println("This is my Second test");
+}
+
+
+@Test
+
+public static void practiceTest3() {
+	System.out.println("This is my Third test");
+}
+
+
+@Test
+
+public static void practiceTest4() {
+	System.out.println("This is my Forth test");
+}
+
+
+
+@Test
+
+public static void practiceTest5() {
+	System.out.println("This is my Fifth test");
+}
+
+@Test
+
+public static void practiceTest6() {
+	System.out.println("This is my Sixth test");
+}
+
+
+	
+
+}
